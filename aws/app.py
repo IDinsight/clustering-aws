04d16ccc818ae2fee1e6a1bbf0af2d4b3b5821f4
amir_emami <amirali1376@gmail.com>
2024-02-10 03:32:39 +0530

    MAJOR: Update app.py code to work.

--- aws/app.py
+++ aws/app.py
@@ -1,5 +1,5 @@
 import time
-
+from subprocess import call
 import awswrangler as wr
 import geopandas as gpd
 
@@ -7,30 +7,32 @@ from clustering.kmeans import (
     custom_kmeans,
     get_oversized_clusters,
     run_optuna_kmeans_study,
-    parallel_kmeans_secondpass,
+    kmeans_secondpass,
 )
+
+# from clustering.kmeans import parallel_kmeans_secondpass
+# secondpass_python_n_jobs = 1
+# secondpass_optuna_n_jobs = 2
 
 # AWS VARIABLES
 input_bucket = "r3-hpls-presampling-barangays"
 output_bucket = "r3-hpls-clustered-barangays"
 
 # CLUSTERING VARIABLES
+# admin variables
 id_col = "grid_id"
 lat_col = "Lat"
 lon_col = "Lon"
 weight_col = "population"
 epsg = 3121  # philippines
+# both passes
+weight_importance_factor = 1
+n_jobs = 2
 # first pass
-firstpass_weight = 240
-firstpass_factor = 1
-firstpass_n_jobs = 2
+desired_weight = 240
 firstpass_n_trials = 96
 # second pass
 secondpass_cutoff_weight = 300
-secondpass_weight = firstpass_weight
-secondpass_factor = firstpass_factor
-secondpass_python_n_jobs = 1
-secondpass_optuna_n_jobs = 2
 secondpass_n_trials = 24
 
 
@@ -38,27 +40,24 @@ def cluster_data(gdf_for_cluster):
 
     # dynamic radius parameter
     if gdf_for_cluster["urban"].iloc[0]:
-        firstpass_radius = 1000
-        secondpass_radius = firstpass_radius
+        desired_radius = 1000
     else:
-        firstpass_radius = 2000
-        secondpass_radius = firstpass_radius
+        desired_radius = 2000
     print(f"Grids in barangay: {len(gdf_for_cluster)}")
 
     # first pass
     study_firstpass = run_optuna_kmeans_study(
         gdf=gdf_for_cluster,
-        desired_cluster_weight=firstpass_weight,
-        desired_cluster_radius=firstpass_radius,
+        desired_cluster_weight=desired_weight,
+        desired_cluster_radius=desired_radius,
         id_col=id_col,
         lat_col=lat_col,
         lon_col=lon_col,
         weight_col=weight_col,
-        weight_importance_factor=firstpass_factor,
+        weight_importance_factor=desired_weight,
         epsg=epsg,
         n_trials=firstpass_n_trials,
-        n_jobs=firstpass_n_jobs,
-        show_progress_bar=True,
+        n_jobs=n_jobs,
     )
 
     # proper run with the best n_cluster
@@ -88,20 +87,19 @@ def cluster_data(gdf_for_cluster):
     ] = 1
 
     # run re-clustering
-    gdf_w_clusters_doublepass = parallel_kmeans_secondpass(
+    gdf_w_clusters_doublepass = kmeans_secondpass(
         gdf_w_clusters=gdf_w_clusters,
         oversized_cluster_ids=oversized_cluster_ids,
-        desired_cluster_weight=secondpass_weight,
-        desired_cluster_radius=secondpass_radius,
+        desired_cluster_weight=desired_weight,
+        desired_cluster_radius=desired_radius,
         id_col=id_col,
         lat_col=lat_col,
         lon_col=lon_col,
         weight_col=weight_col,
-        weight_importance_factor=secondpass_factor,
+        weight_importance_factor=weight_importance_factor,
         epsg=epsg,
         n_trials=secondpass_n_trials,
-        python_n_jobs=secondpass_python_n_jobs,
-        optuna_n_jobs=secondpass_optuna_n_jobs,
+        n_jobs=n_jobs,
     )
     gdf_w_clusters_doublepass = gdf_w_clusters_doublepass.sort_values(by="cluster_id")
 
@@ -121,24 +119,35 @@ def handler(event, context):
     # Download file from S3
     input_filename = event["filename"]
     output_filename = f"clustered_{input_filename}"
+
     try:
-        gdf_for_cluster = download_data(input_bucket, output_filename)
+        print("Downloading...")
+        gdf_for_cluster = download_data(input_bucket, input_filename)
     except Exception:
-        # retry once if fails
-        time.sleep(3)
-        gdf_for_cluster = download_data(input_bucket, output_filename)
+        try:
+            print("Retrying download...")
+            time.sleep(3)
+            gdf_for_cluster = download_data(input_bucket, output_filename)
+        except Exception as e:
+            raise Exception(e)
 
     # Cluster
-    gdf_w_clusters = cluster_data(gdf_for_cluster)
-    gdf_w_clusters.to_parquet(f"/tmp/{output_filename}")
+    try:
+        print("Clustering...")
+        gdf_w_clusters = cluster_data(gdf_for_cluster)
+        gdf_w_clusters.to_parquet(f"/tmp/{output_filename}")
+    except Exception as e:
+        raise Exception(e)
 
     # Upload file to S3
-    wr.s3.upload(
-        local_file=f"/tmp/{output_filename}",
-        path=f"s3://{output_bucket}/{output_filename}",
-    )
+    try:
+        print("Uploading...")
+        wr.s3.upload(
+            local_file=f"/tmp/{output_filename}",
+            path=f"s3://{output_bucket}/{output_filename}",
+        )
+    except Exception as e:
+        raise Exception(e)
 
     # clean up because some resources are shared across executions
-    from subprocess import call
-
     call("rm -rf /tmp/*", shell=True)
